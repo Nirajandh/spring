5cbbd82cca090e886844a2424c7d7028b306efeb
Nirajandh <151948730+Nirajandh@users.noreply.github.com>
2023-11-30 23:30:37 +0530

    Add files via upload

---
+++ SetterInjection/App.java
@@ -0,0 +1,38 @@
+package com.SpringCore;
+
+
+
+import javax.naming.Context;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ApplicationContext app=new ClassPathXmlApplicationContext("config.xml");
+       Student std1=(Student) app.getBean("student1");
+        
+        System.out.println(std1);
+  Student std2=(Student) app.getBean("student2");
+        
+        System.out.println(std2);
+        
+        // using p schema
+ Student std3=(Student) app.getBean("student3");
+        
+        System.out.println(std3);
+        
+    }
+}
+
+
+// property addition 
+// recommend to use the p schema type becasuse easy to write
